64b79d9d8e3c6f6bc5774d2affdc70f3697401b8
mohamad-gandomi <m.gandomi1995@gmail.com>
2023-08-22 13:57:48 +0330

    Add plugin comment

---
+++ magiz-dash-post.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Plugin Name:       Magiz Dash Post
+ * Plugin URI:        https://magiztech.com/plugins/magiz-dash-post/
+ * Description:       Create a user front dashboard panel and create or updare posts
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mohamad Gandomi
+ * Author URI:        https://magiztech.com/mohamad-gandomi
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       magiz-dash-post
+ * Domain Path:       /languages
+ */
