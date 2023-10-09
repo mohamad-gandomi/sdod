a459e79abc7498056befa8429c1cc2fe4c538e39
mohamad-gandomi <m.gandomi1995@gmail.com>
2023-10-09 14:37:50 +0330

    add wells shortcode table

--- public/magiz_wells_shortcode.php
+++ public/magiz_wells_shortcode.php
@@ -26,42 +26,27 @@ function magiz_wells_shortcode($atts) {
 		</thead>
 		<!-- Responsive Table Body Section -->
 		<tbody class="responsive-table__body">
-			<tr class="responsive-table__row">
-				<td class="responsive-table__body__text responsive-table__body__text--no">1</td>
-				<td class="responsive-table__body__text responsive-table__body__text--mastno">1</td>
-				<td class="responsive-table__body__text responsive-table__body__text--location">Ahvaz</td>
-				<td class="responsive-table__body__text responsive-table__body__text--system">APS</td>
-				<td class="responsive-table__body__text responsive-table__body__text--status"><span class="status-indicator status-indicator--active"></span>Active</td>
-				<td class="responsive-table__body__text responsive-table__body__text--personle">Gandomi</td>
-				<td class="responsive-table__body__text responsive-table__body__text--action"><a href="#">link</a></td>
-			</tr>
-			<tr class="responsive-table__row">
-				<td class="responsive-table__body__text responsive-table__body__text--row">1</td>
-				<td class="responsive-table__body__text responsive-table__body__text--mastno">1</td>
-				<td class="responsive-table__body__text responsive-table__body__text--location">Ahvaz</td>
-				<td class="responsive-table__body__text responsive-table__body__text--system">APS</td>
-				<td class="responsive-table__body__text responsive-table__body__text--status"><span class="status-indicator status-indicator--active"></span>Active</td>
-				<td class="responsive-table__body__text responsive-table__body__text--personle">Gandomi</td>
-				<td class="responsive-table__body__text responsive-table__body__text--action"><a href="#">link</a></td>
-			</tr>
-			<tr class="responsive-table__row">
-				<td class="responsive-table__body__text responsive-table__body__text--row">1</td>
-				<td class="responsive-table__body__text responsive-table__body__text--mastno">1</td>
-				<td class="responsive-table__body__text responsive-table__body__text--location">Ahvaz</td>
-				<td class="responsive-table__body__text responsive-table__body__text--system">APS</td>
-				<td class="responsive-table__body__text responsive-table__body__text--status"><span class="status-indicator status-indicator--active"></span>Active</td>
-				<td class="responsive-table__body__text responsive-table__body__text--personle">Gandomi</td>
-				<td class="responsive-table__body__text responsive-table__body__text--action"><a href="#">link</a></td>
-			</tr>
-			<tr class="responsive-table__row">
-				<td class="responsive-table__body__text responsive-table__body__text--row">1</td>
-				<td class="responsive-table__body__text responsive-table__body__text--mastno">1</td>
-				<td class="responsive-table__body__text responsive-table__body__text--location">Ahvaz</td>
-				<td class="responsive-table__body__text responsive-table__body__text--system">APS</td>
-				<td class="responsive-table__body__text responsive-table__body__text--status"><span class="status-indicator status-indicator--active"></span>Active</td>
-				<td class="responsive-table__body__text responsive-table__body__text--personle">Gandomi</td>
-				<td class="responsive-table__body__text responsive-table__body__text--action"><a href="#">link</a></td>
-			</tr>
+			<?php
+            // Get all wells
+            $wells = get_posts([
+                'post_type' => 'well',
+            ]);
+
+            // Loop through the wells and display them in the table
+            foreach ($wells as $well) {
+                ?>
+                <tr class="responsive-table__row">
+                    <td class="responsive-table__body__text responsive-table__body__text--no"><?php echo $well->ID; ?></td>
+                    <td class="responsive-table__body__text responsive-table__body__text--mastno"><?php echo 1;//get_post_meta($well->ID, 'mast_no', true); ?></td>
+                    <td class="responsive-table__body__text responsive-table__body__text--location"><?php echo get_post_meta($well->ID, 'field-loc', true); ?></td>
+                    <td class="responsive-table__body__text responsive-table__body__text--system"><?php echo 'APS';//get_post_meta($well->ID, 'system', true); ?></td>
+                    <td class="responsive-table__body__text responsive-table__body__text--status"><span class="status-indicator status-indicator--active"></span>Active</td>
+                    <td class="responsive-table__body__text responsive-table__body__text--personle"><?php echo 'ali';//get_post_meta($well->ID, 'personel', true); ?></td>
+                    <td class="responsive-table__body__text responsive-table__body__text--action"><a href="#">link</a></td>
+                </tr>
+                <?php
+            }
+            ?>
 		</tbody>
 	</table>
 </div>
